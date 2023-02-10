0457be6816fab0cc85304c4101e3a726c16f5886
RUKtheCROOK <johngarcia053@gmail.com>
2023-02-09 20:00:37 -0800

    Class-3-Update
    This is the update for my code after class 3. I have added the categories section. I only have 2 so it only shows fruits and vegetables. I tested it however and it worked flawlessly.

--- data.py
+++ data.py
@@ -9,3 +9,58 @@ me = {"name": "John",
         "zipcode": "12345",
         "city": "New York",
 }}
+
+
+mock_database = [
+{
+    "title": "apples",
+    "category": "fruit",
+    "price": float("16.25"),
+    "image": "/images/apples.png",
+    "_id": "1",
+  },
+  {
+    "title": "oranges",
+    "category": "fruit",
+    "price": float("1.3"),
+    "image": "/images/oranges.png",
+    "_id": "2",
+  },
+  {
+    "title": "bananas",
+    "category": "fruit",
+    "price": float("1.4"),
+    "image": "/images/bananas.png",
+    "_id": "3",
+  },
+  {
+    "title": "pears",
+    "category": "fruit",
+    "price": float("1.5"),
+    "image": "/images/pears.png",
+    "_id": "4",
+  },
+  {
+    "title": "grapes",
+    "category": "fruit",
+    "price": float("1.6"),
+    "image": "/images/grapes.png",
+    "_id": "5",
+  },
+  {
+    "title": "carrots",
+    "category": "vegetable",
+    "price": float("1.7"),
+    "image": "/images/carrots.png",
+    "_id": "6",
+  },
+  {
+    "title": "celery",
+    "category": "vegetable",
+    "price": float("1.8"),
+    "image": "/images/celery.png",
+    "_id": "7",
+  }
+]
+
+print(mock_database)
--- server.py
+++ server.py
@@ -1,6 +1,6 @@
 # import the package
-from flask import Flask
-from data import me
+from flask import Flask, abort
+from data import me, mock_database
 import json
 
 app = Flask(__name__)
@@ -17,7 +17,62 @@ def about():
 def contactme():
     return "MyEmailAddress@gmail.com"
 
+@app.get("/api/products")
+def products():
+    # string = mock_database
+    # apples = string[0]["title"],string[0]["category"],string[0]["price"]
+    # oranges = string[1]["title"],string[1]["category"],string[1]["price"]
+    # bananas = string[2]["title"],string[2]["category"],string[2]["price"]
+    # pears = string[3]["title"],string[3]["category"],string[3]["price"]
+    # grapes = string[4]["title"],string[4]["category"],string[4]["price"]
+    # carrots = string[5]["title"],string[5]["category"],string[5]["price"]
+    # celery = string[6]["title"],string[6]["category"],string[6]["price"]
+    # return f'{apples,oranges,bananas,pears,grapes,carrots,celery}'
+    return json.dumps(mock_database)
 
+@app.get("/api/products/count")
+def products_count():
+    length = len(mock_database)
+    return json.dumps(length)
+
+@app.get("/api/categories/<category>")
+def categories(category):
+    # in class
+    # results = []
+    # for product in mock_database:
+    #     if product["category"] == category:
+    #         results.append(product)
+    # return json.dumps(results)
+    filtered = list(filter(lambda x: x["category"] == category, mock_database))
+    return json.dumps(filtered)
+
+@app.get("/api/product/<_id>")
+def product(_id):
+    filtered = list(filter(lambda x: x["_id"] == _id, mock_database))
+    if len(filtered) == 0:
+            # not_found = (int(404), "Not Found")
+            # return json.dumps(not_found)
+            return abort(404, "Not found")
+    else:
+            return json.dumps(filtered)
+
+@app.get("/api/product/search/<search>")
+def product_search(search):
+    filtered = list(filter(lambda x: search.lower() in x["title"].lower(), mock_database))
+    if len(filtered) == 0:
+            # not_found = (int(404), "Not Found")
+            # return json.dumps(not_found)
+            return abort(404, "Not found")
+    else:
+            return json.dumps(filtered)
+
+@app.get("/api/unique_categories")
+def unique_categories():
+    categories = []
+    for product in mock_database:
+        if product["category"] not in categories:
+            categories.append(product["category"])
+    return json.dumps(categories)
 
 # ###############################################
 # api -> json
@@ -30,7 +85,6 @@ def dev():
 @app.get("/api/dev/address")
 def dev_address():
     string = me["address"]
-    # return string ["street"] + str(string["number"]) + string["zipcode"] + string["city"]
     return f'{string["street"]} #{string["number"]}, {string["zipcode"]} {string["city"]}'
 
 app.run(debug=True)
